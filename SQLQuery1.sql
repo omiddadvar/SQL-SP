a8d9ce72121a99a2c8f6f0240ab4de27c6a4772d
Omid Dadvar <Dadvar@fanni.tazarv.com>
2021-04-06 15:35:38 +0430

    initial :wink

---
+++ SQLQuery1.sql
@@ -0,0 +1,26 @@
+create proc dbo.spGreen_Zanjan @MediaTypeId int,
+	@AreaIDs as nvarchar(500),
+	@FromDate as nvarchar(10),
+	@ToDate as nvarchar(10),
+	@ExternalServiceIDs as nvarchar(500),
+	@EndJobStateIDs as nvarchar(500)
+as
+begin
+end
+
+
+select * from Tbl_ExternalService order by ExternalServiceId asc
+go
+select * from Tbl_ErjaState order by ErjaStateId asc
+go
+select * from Tbl_EndJobState order by EndJobStateId asc
+go
+select * from Tbl_MediaType order by MediaTypeId asc
+
+	
+execute spAAAAa @MediaTypeId = 5 ,
+	@AreaIDs = '2,3,5',
+	@FromDate = '1399/04/05',
+	@ToDate = '1400/01/01',
+	@ExternalServiceIDs = '1,2',
+	@EndJobStateIDs = '1,2'
