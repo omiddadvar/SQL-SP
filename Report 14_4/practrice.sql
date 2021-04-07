fb01c2b5f3f1c419f3da6484685c34b465c53380
Omid Dadvar <Dadvar@fanni.tazarv.com>
2021-04-07 17:20:53 +0430

    SQl Not finished :weary:

--- Report 14_4/practrice.sql
+++ Report 14_4/practrice.sql
@@ -14,14 +14,21 @@ select top(5) * from TblRequestData
 go
 select * from Tbl_ExternalService order by ExternalServiceId asc
 go
-select * from Tbl_ErjaState order by ErjaStateId asc
-go
-select * from Tbl_EndJobState order by EndJobStateId asc
-go
 select * from Tbl_MediaType order by MediaTypeId asc
 go
 select top(10) * from Tbl_GISSMSRecevied
-
+go
+select top(5) * from TblRequest
+go
+select data.MediaTypeId , data.ExternalServiceId , erja.* from TblErjaRequest erja
+	inner join TblRequest req on req.RequestId = erja.RequestId
+	left JOIN TblRequestData data on data.RequestId = req.RequestId
+	where req.RequestNumber = 99992349
+go
+select * from Tbl_ErjaState order by ErjaStateId asc
+go	
+select * from Tbl_EndJobState order by EndJobStateId asc
+go
 	
 --execute spAAAAa @MediaTypeId = 5 ,
 --	@AreaIDs = '2,3,5',
@@ -33,4 +40,3 @@ select top(10) * from Tbl_GISSMSRecevied
 
 
 execute spAAAAa 0, '2,3,4,5,6,7,8,9,10' , '1399/01/14',  '1400/01/14' , ''
-
