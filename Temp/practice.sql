572a0e23e14211eb08189e4422847939015b0d01
Omid Dadvar <Dadvar@fanni.tazarv.com>
2021-04-19 12:13:40 +0430

    adding Temp Folder :grin:

---
+++ Temp/practice.sql
@@ -0,0 +1,9 @@
+--select top(100) * from Tbl_LPFeeder where FeederCode IS NOT Null and LEN(FeederCode) > 0
+
+select top(2) * from Tbl_LPFeeder where LPFeederCode is not null
+select top(2) * from Tbl_LPPost
+
+select top(2) * from TblLPPostLoad
+select top(2) * from TblLPFeederLoad
+
+select top(2) * from Tbl_Fuse
