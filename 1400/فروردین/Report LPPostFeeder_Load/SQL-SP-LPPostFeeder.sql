5a4ef2a8ba2dcc261d1a3d969821350b8f53ca9d
Omid Dadvar <Dadvar@fanni.tazarv.com>
2021-04-28 16:42:44 +0430

    Tiny changes :grin:

--- 1400/فروردین/Report LPPostFeeder_Load/SQL-SP-LPPostFeeder.sql
+++ 1400/فروردین/Report LPPostFeeder_Load/SQL-SP-LPPostFeeder.sql
@@ -5,7 +5,7 @@ IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'[dbo].[Sp-PostFee
   DROP PROCEDURE [dbo].[Sp-PostFeederSelect] 
 GO
 CREATE PROCEDURE [dbo].[Sp-PostFeederSelect] 
-	@feederIds as NVARCHAR(1000),
+	@feederIds as VARCHAR(1000),
 	@state as INT
 AS
 BEGIN
@@ -35,53 +35,3 @@ BEGIN
 	EXEC(@lsql)
 END
 GO
-
-exec [dbo].[Sp-PostFeederSelect] '20050983,20051023,20054050,
-	20051067,20054053', 1;
-
-exec [dbo].[Sp-PostFeederSelect] '20050983,20051023,20054050,
-	20051067,20054053' , 2;
-
-
-
-exec [dbo].[Sp-PostFeederSelect] '20050983,20051023' , 3;
-
-select * from Tbl_LPFeeder where LPFeederId = 20050983
-select * from TblLPFeederLoad where LPFeederId = 9900000990180523
-
-select TOP(10) * from TblLPFeederLoad where EarthValue IS Not NULL
-
-select TOP(10) * from Tbl_LPFeeder where LPFeederId IN ('20050983','20051023')
-
-select  Count(*) from TblLPPostLoad 
-
-select TOP(10) * from Tbl_LPPost 
-
-delete from TblLPPostLoad where LPPostId Is Null
-
-SELECT COLUMN_NAME
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'TblLPPostLoad'
-ORDER BY ORDINAL_POSITION
-
-select top(2) * from TblLPFeederLoad
-select top(2) * from TblLPPostLoad where LPPostId Is Null
-
-
-
-Select f.* , fl.LPFeederLoadId from  Tbl_LPFeeder f
-Inner join TblLPFeederLoad fl on f.LPFeederId = fl.LPFeederId
-WHERE fl.LPFeederId IN ('20050983','20051023')
-
-Select f.* from  Tbl_LPFeeder f
-WHERE f.LPFeederId IN ('20050983','20051023')
-
-Select top(20) * from  Tbl_LPFeeder where LPFeederCode Is NOT NULL Order by LPFeederId desc
-exec [dbo].[Sp-PostFeederSelect] '20054589', 1;
-exec [dbo].[Sp-PostFeederSelect] '20054589' , 2;
-select * from Tbl_LPPost where LPPostId = 10050844
-
-
-
-SELECT PostPeakCurrent As Peak , * FROM Tbl_LPPost WHERE LPPostId IN (10050860,20051020);
-SELECT FeederPeakCurrent As Peak , * FROM Tbl_LPFeeder WHERE LPFeederId IN ('20050983','20051023')
