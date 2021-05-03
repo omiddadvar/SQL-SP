961926a9df6ea5cb1aa2b8d4bba8965d1fe006b4
Omid Dadvar <Dadvar@fanni.tazarv.com>
2021-05-03 10:45:21 +0430

    Caching :wink:

---
+++ Temp/SQLQuery2.sql
@@ -0,0 +1,9 @@
+use CcRequesterSetad
+go
+select top(2) * from TblRequestPart
+go
+select top(2) * from TblMPRequestPart
+go
+select top(2) * from TblLPRequestPart
+
+select top(2) * from TblLightRequestPart
