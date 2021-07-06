f5e3e753e478eef2b5bfb19e7a7df91b5eea29ab
Omid Dadvar <Dadvar@fanni.tazarv.com>
2021-07-06 17:04:28 +0430

    Worried not to fuck-up :worried:

---
+++ Stored Procedures/App/04_تیر/New folder/sp 01.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE spOmid 
+  @aAreaIDs AS NVARCHAR(200),
+  @aMasterIDs AS NVARCHAR(200),
+  @aTabletIDs AS NVARCHAR(200)
+  AS
+  BEGIN
+    /*TODO : Implement SP !!!*/
+  END
---
+++ Temp/Trace.sql
@@ -0,0 +1,55 @@
+SELECT
+		 distinct [TblOnCall].[OnCallId]
+	  into #tmpOnCall
+	  FROM [CCRequesterSetad].[Homa].[TblTraceSummary]
+	  inner join [CCRequesterSetad].[Homa].[TblOnCall]
+	  on [TblTraceSummary].OnCallId=[TblOnCall].OnCallId
+	  where TargetDatePersian='1400/04/13'
+	  and MasterId=20050975
+	  
+select Homa.TblTrace.*,CAST(0 as bit) as IsFindJob into #tmp from Homa.TblTrace where OnCallId in (
+	  SELECT
+		 [OnCallId]	      
+	  FROM #tmpOnCall
+  )
+  order by OnCallId,TraceDT
+  
+  select * into #tmpEkipArrive from Homa.TblEkipArrive 
+  where OnCallId in (
+  SELECT
+		 [OnCallId]	      
+	  FROM #tmpOnCall
+	  
+  )
+  select * into #tmpStartMove from Homa.TblEkipStartMove
+  where OnCallId in (
+	  SELECT
+		 [OnCallId]	      
+	  FROM #tmpOnCall
+  )
+  --select * from #tmpStartMove
+  --select * from #tmpEkipArrive
+  select #tmpStartMove.OnCallId,#tmpStartMove.JobId, #tmpStartMove.StartMoveDT,#tmpEkipArrive.ArriveDT  
+  into #tmpFind
+  from #tmpStartMove 
+	inner join #tmpEkipArrive on #tmpStartMove.OnCallId=#tmpEkipArrive.OnCallId
+	and #tmpStartMove.JobId=#tmpEkipArrive.JobId
+  update #tmp
+  set IsFindJob=1
+  from  #tmp
+  inner join #tmpFind
+  on #tmpFind.OnCallId=#tmp.OnCallId
+  where TraceDT >=#tmpFind.StartMoveDT
+  and TraceDT <=#tmpFind.ArriveDT
+  
+	
+  /*update #tmp
+  set IsFindJob=1
+  from (*/
+  
+  select * from #tmp
+  drop table #tmpOnCall
+  drop table #tmpFind
+  drop table #tmpStartMove
+  drop table #tmpEkipArrive
+  drop table #tmp
