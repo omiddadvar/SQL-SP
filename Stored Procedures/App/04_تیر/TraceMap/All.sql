4f5f0c6a568962a58c22dd3941e7986a0442bf06
Omid Dadvar <Dadvar@fanni.tazarv.com>
2021-07-18 17:00:08 +0430

    some SQL problems :triumph:

--- Stored Procedures/App/04_تیر/TraceMap/All.sql
+++ Stored Procedures/App/04_تیر/TraceMap/All.sql
@@ -669,7 +669,7 @@ CREATE PROCEDURE Homa.spGetStateTrace
 
       /* Start Times */
       INSERT INTO #tmpDT SELECT tesm.StartMoveDT , 'Start'  FROM  Homa.TblJob tj
-      INNER JOIN Homa.TblEkipStartMove tesm ON tj.OncallId = tesm.OnCallId
+      INNER JOIN Homa.TblEkipStartMove tesm ON tj.JobId = tesm.JobId
       WHERE tj.RequestId = @RequestId
 
       /* Arrive Times */
@@ -683,7 +683,7 @@ CREATE PROCEDURE Homa.spGetStateTrace
       WHERE tj.RequestId = @RequestId
         
       /*Get TraceStates */
-        DECLARE db_cursor CURSOR FOR SELECT * FROM #tmpDT d
+        DECLARE db_cursor CURSOR FOR SELECT * FROM #tmpDT
         DECLARE @lDate DATETIME , @lFlag VARCHAR(10)
         OPEN db_cursor  
         FETCH NEXT FROM db_cursor INTO @lDate , @lFlag
@@ -703,80 +703,4 @@ CREATE PROCEDURE Homa.spGetStateTrace
       DROP TABLE #tmpResult
       DROP TABLE #tmpDT
       DROP TABLE #tmpTrace
-  END
-
-CREATE PROCEDURE Homa.spGetStateTrace
-  @RequestId AS BIGINT
-  ,@OnCallId AS BIGINT
-  AS 
-  BEGIN
-  	CREATE TABLE #tmpResult
-    	(
-    		Radif INT,
-    		TraceDT DATETIME,
-    		GpsX FLOAT,
-    		GpsY FLOAT,
-        Flag VARCHAR(10)
-    	)
-      CREATE TABLE #tmpTrace
-    	(
-    		Radif INT,
-    		TraceDT DATETIME,
-    		GpsX FLOAT,
-    		GpsY FLOAT
-    	)
-     CREATE TABLE #tmpDT
-    	(
-    		TraceDT DATETIME,
-        Flag VARCHAR(10)
-    	)
-      INSERT INTO  #tmpTrace EXEC Homa.spGetTrace @OnCallId
-
-      /* Start Times */
-      INSERT INTO #tmpDT SELECT tesm.StartMoveDT , 'Start'  FROM  Homa.TblJob tj
-      INNER JOIN Homa.TblEkipStartMove tesm ON tj.OncallId = tesm.OnCallId
-      WHERE tj.RequestId = @RequestId
-
-      /* Arrive Times */
-      INSERT INTO #tmpDT SELECT tea.ArriveDT , 'Arrive'  FROM  Homa.TblJob tj
-      INNER JOIN Homa.TblEkipArrive tea ON tj.JobId = tea.JobId
-      WHERE tj.RequestId = @RequestId
-
-      /* Finish Times */
-      INSERT INTO #tmpDT SELECT tr.ConnectDT , 'Finish'  FROM  Homa.TblJob tj
-      INNER JOIN dbo.TblRequest tr ON tr.RequestId = tj.RequestId
-      WHERE tj.RequestId = @RequestId
-        
-      /*Get TraceStates */
-        DECLARE db_cursor CURSOR FOR SELECT * FROM #tmpDT d
-        DECLARE @lDate DATETIME , @lFlag VARCHAR(10)
-        OPEN db_cursor  
-        FETCH NEXT FROM db_cursor INTO @lDate , @lFlag
-        
-        WHILE @@FETCH_STATUS = 0  
-        BEGIN  
-              INSERT INTO #tmpResult SELECT  TOP 1 * , @lFlag FROM #tmpTrace
-                ORDER BY ABS(DATEDIFF(SECOND , @lDate , TraceDT))
-              FETCH NEXT FROM db_cursor INTO @lDate , @lFlag
-        END 
-        
-        CLOSE db_cursor  
-        DEALLOCATE db_cursor 
-      
-      SELECT * FROM #tmpResult
-      
-      DROP TABLE #tmpResult
-      DROP TABLE #tmpDT
-      DROP TABLE #tmpTrace
-  END
-
-CREATE PROCEDURE Homa.spOnCallInfo
-  @OnCallId BIGINT
-  AS
-  BEGIN
-  	SELECT O.OnCallId, O.MasterId , O.TabletId , A.AreaId , M.Name AS MasterName, T.TabletName FROM Homa.TblOnCall O
-  INNER JOIN Tbl_AreaUser A ON O.AreaUserId = A.AreaUserId
-  INNER JOIN Homa.Tbl_Tablet T ON O.TabletId = T.TabletId
-  INNER JOIN Tbl_Master M ON O.MasterId = M.MasterId
-  WHERE O.OnCallId = @OnCallId
   END
