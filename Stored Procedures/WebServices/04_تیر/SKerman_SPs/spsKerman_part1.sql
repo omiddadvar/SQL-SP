a1cb15053a860397953c8b8be5adf2c0426a86ba
Omid Dadvar <Dadvar@fanni.tazarv.com>
2021-07-25 11:27:58 +0430

    SKerman 4 WebServices are ready to launch :triumph:

--- Stored Procedures/WebServices/04_تیر/SKerman_SPs/spsKerman_part1.sql
+++ Stored Procedures/WebServices/04_تیر/SKerman_SPs/spsKerman_part1.sql
@@ -9,22 +9,23 @@ CREATE PROCEDURE spSKerman_part1
     DECLARE @TOYM VARCHAR(10) = SUBSTRING( @To, 1, 7)
     DECLARE @Days INT = DATEDIFF(DAY , dbo.shtom(@From) , dbo.shtom(@TO))
     DECLARE @CntAllPublicMPFeeders AS INT = (SELECT COUNT(*) FROM Tbl_MPFeeder WHERE ISNULL(OwnershipId,2) = 2)
+    DECLARE @CntAllPrivateMPFeeders AS INT = (SELECT COUNT(*) FROM Tbl_MPFeeder WHERE OwnershipId = 1)
 	  SELECT RequestId, MPRequestId , LPRequestId INTO #tmp FROM TblRequest WHERE DisconnectDatePersian BETWEEN @From AND @To
     
-    SELECT ta.AreaId ,ta.Area ,ISNULL(TMP.DisPowerMP_Tamir,0) AS DisPowerMP_Tamir 
+    SELECT ta.AreaId ,ta.Area ,ROUND(ISNULL(TMP.DisPowerMP_Tamir,0),2) AS DisPowerMP_Tamir 
     ,ISNULL(TMP.DisIntervalMP_Tamir,0) AS DisIntervalMP_Tamir
-	  ,ISNULL(TMP.DisPowerMP,0) AS DisPowerMP, ISNULL(TMP.DisIntervalMP,0) AS DisIntervalMP
-    ,ISNULL(TMP.CountMP_Tamir,0) AS CountMP_Tamir, ISNULL(TMP.CountMP,0) AS CntMP
-	  ,ISNULL(TMP.DisAvgMP,0) AS DisAvgMP ,ISNULL(TMP.CountMPFromPost_Tamir,0) AS CntMPFromPost_Tamir 
-    ,ISNULL(TMP.CountMPFromPost,0) AS CountMPFromPost ,ISNULL(TLP.DisPowerLP_Tamir,0) AS DisPowerLP_Tamir
-	  ,ISNULL(TLP.DisIntervalLP_Tamir,0) AS DisIntervalLP_Tamir, ISNULL(TLP.DisPowerLP,0) AS DisPowerLP
+	  ,ROUND(ISNULL(TMP.DisPowerMP,0),2) AS DisPowerMP, ISNULL(TMP.DisIntervalMP,0) AS DisIntervalMP
+    ,ISNULL(TMP.CountMP_Tamir,0) AS CntMP_Tamir, ISNULL(TMP.CountMP,0) AS CntMP
+	  ,ROUND(ISNULL(TMP.DisAvgMP,0),2) AS DisAvgMP , ROUND(ISNULL( TLP.DisAvgLP,0),2) AS DisAvgLP
+    ,ISNULL(TMP.CountMPFromPost,0) AS CntMPFromPost ,ROUND(ISNULL(TLP.DisPowerLP_Tamir,0),2) AS DisPowerLP_Tamir
+	  ,ISNULL(TLP.DisIntervalLP_Tamir,0) AS DisIntervalLP_Tamir, ROUND(ISNULL(TLP.DisPowerLP,0),2) AS DisPowerLP
 	  ,ISNULL(TLP.DisIntervalLP,0) AS DisIntervalLP ,ISNULL(TLP.CountLP_Tamir,0) AS CntLP_Tamir
-	  ,ISNULL(TLP.CountLP,0) AS CountLP, ISNULL( TLP.DisAvgLP,0) AS DisAvgLP
-	  ,ISNULL(TAvgMP.AverageDisInterval,0) AS AvgDisIntervalMP , ISNULL(TAvgLP.AverageDisInterval,0) AS AvgDisIntervalLP
+	  ,ISNULL(TLP.CountLP,0) AS CntLP ,ISNULL(TMP.CountMPFromPost_Tamir,0) AS CntMPFromPost_Tamir 
     ,ISNULL(FeederReq.FeederCount,0) AS FeederCnt ,ISNULL(ROUND(FeederReq.FeederPercent,2),0) AS FeederPercent
+    ,@CntAllPublicMPFeeders AS cntPublicMPFeeder , @CntAllPrivateMPFeeders AS cntPrivateMPFeeder
     ,ISNULL(FaultyFeeder.MPFeederName , '') AS FaultyFeederName
-    ,ISNULL(SAIDI.DisPowTamir,0) AS SAIDI_Tamir ,ISNULL(SAIDI.DisPow,0) AS SAIDI
-    ,ISNULL(Rate.Rate,0) AS Rate ,ISNULL(Rate.DisIntRate,0) AS DisRate
+    ,ROUND(ISNULL(SAIDI.DisPowTamir,0),2) AS SAIDI_Tamir ,ROUND(ISNULL(SAIDI.DisPow,0),2) AS SAIDI
+    ,ROUND(ISNULL(Rate.Rate,0),2) AS Rate ,ROUND(ISNULL(Rate.DisIntRate,0),2) AS DisRate
       FROM
     Tbl_Area ta LEFT JOIN(
     SELECT R.AreaId, SUM( CASE WHEN R.IsTamir = 1 THEN R.DisconnectPower END ) AS DisPowerMP_Tamir
@@ -33,8 +34,8 @@ CREATE PROCEDURE spSKerman_part1
         , SUM( CASE WHEN R.IsTamir = 0 AND R.DisconnectInterval >= 5  THEN R.DisconnectPower END ) AS DisPowerMP
         , SUM( CASE WHEN R.IsTamir = 0 AND R.DisconnectInterval >= 5 THEN R.DisconnectInterval END ) AS DisIntervalMP
         , COUNT( CASE WHEN R.IsTamir = 0 AND R.DisconnectInterval >= 5 THEN R.RequestId END ) AS CountMP
-        , SUM(CASE WHEN R.IsTamir = 0 THEN MP.DisconnectInterval END) /
-            COUNT(CASE WHEN R.IsTamir = 0 THEN MP.MPRequestId END) AS DisAvgMP
+        , CAST(SUM(CASE WHEN R.IsTamir = 0 THEN R.DisconnectInterval END) AS FLOAT) /
+            COUNT(CASE WHEN R.IsTamir = 0 THEN R.RequestId END) AS DisAvgMP
         , COUNT( CASE WHEN R.IsTamir = 1 AND R.IsDisconnectMPFeeder = 1 THEN R.RequestId END ) AS CountMPFromPost_Tamir
         , COUNT( CASE WHEN R.IsTamir = 0 AND R.IsDisconnectMPFeeder = 1 THEN R.RequestId END ) AS CountMPFromPost 
       FROM TblMPRequest MP
@@ -42,7 +43,7 @@ CREATE PROCEDURE spSKerman_part1
         INNER JOIN #tmp ON R.RequestId = #tmp.RequestId
         WHERE (MP.DisconnectReasonId IS NULL OR MP.DisconnectReasonId < 1200 OR MP.DisconnectReasonId > 1299 ) 
         AND (MP.DisconnectGroupSetId IS NULL OR MP.DisconnectGroupSetId <> 1129 AND MP.DisconnectGroupSetId <> 1130)
-        AND R.DisconnectInterval >= 5 AND R.DisconnectDatePersian BETWEEN @From AND @To
+        AND R.DisconnectInterval >= 5
         GROUP BY  R.AreaId ) TMP ON ta.AreaId = TMP.AreaId
     LEFT JOIN(
       SELECT R.AreaId, SUM(CASE WHEN R.IsTamir = 1 THEN R.DisconnectPower END) AS DisPowerLP_Tamir
@@ -51,8 +52,8 @@ CREATE PROCEDURE spSKerman_part1
         , SUM(CASE WHEN R.IsTamir = 0 THEN R.DisconnectPower END) AS DisPowerLP
         , SUM(CASE WHEN R.IsTamir = 0 THEN R.DisconnectInterval END) AS DisIntervalLP
         , COUNT(CASE WHEN R.IsTamir = 0 THEN R.RequestId END) AS CountLP
-        , SUM(CASE WHEN R.IsTamir = 0 THEN LP.DisconnectInterval END) /
-            COUNT(CASE WHEN R.IsTamir = 0 THEN LP.LPRequestId END) AS DisAvgLP
+        , CAST(SUM(CASE WHEN R.IsTamir = 0 THEN R.DisconnectInterval END) AS FLOAT) /
+            COUNT(CASE WHEN R.IsTamir = 0 THEN R.RequestId END) AS DisAvgLP
       FROM TblLPRequest LP
         INNER JOIN TblRequest R ON LP.LPRequestId = R.LPRequestId
         INNER JOIN #tmp ON R.RequestId = #tmp.RequestId
@@ -60,8 +61,8 @@ CREATE PROCEDURE spSKerman_part1
         ) TLP ON ta.AreaId = TLP.AreaId
     LEFT JOIN(
       SELECT R.AreaId
-        ,1000 * SUM(R.DisconnectPower) / SUM(Sub.Energy) AS Rate
-        ,24 * 60 * SUM(R.DisconnectInterval) / SUM(Sub.Energy) AS DisIntRate
+        ,1000 * CAST(SUM(R.DisconnectPower) AS FLOAT) / SUM(Sub.Energy) AS Rate
+        ,24 * 60 * CAST(SUM(R.DisconnectInterval) AS FLOAT) / SUM(Sub.Energy) AS DisIntRate
       FROM TblRequest R
         INNER JOIN TblMPRequest MP ON R.MPRequestId = MP.MPRequestId
         INNER JOIN #tmp ON R.RequestId = #tmp.RequestId
@@ -72,22 +73,6 @@ CREATE PROCEDURE spSKerman_part1
             AND MP.DisconnectGroupSetId <> 1130)))
         GROUP BY R.AreaId
         ) Rate ON ta.AreaId = Rate.AreaId
-    LEFT JOIN(
-      SELECT R.AreaId, SUM(R.DisconnectInterval) / COUNT(R.RequestId) AS AverageDisInterval
-      FROM TblMPRequest MP
-        INNER JOIN TblRequest R ON MP.MPRequestId = R.MPRequestId
-        INNER JOIN #tmp ON R.RequestId = #tmp.RequestId
-        WHERE R.IsTamir = 0
-        GROUP BY R.AreaId
-        ) TAvgMP ON ta.AreaId = TAvgMP.AreaId
-    LEFT JOIN(
-      SELECT R.AreaId, SUM(R.DisconnectInterval) / COUNT(R.RequestId) AS AverageDisInterval
-      FROM TblLPRequest LP
-        INNER JOIN TblRequest R ON LP.LPRequestId = R.LPRequestId
-        INNER JOIN #tmp ON R.RequestId = #tmp.RequestId
-        WHERE R.IsTamir = 0
-        GROUP BY R.AreaId
-        ) TAvgLP ON ta.AreaId = TAvgLP.AreaId
     LEFT JOIN(
       SELECT MP.AreaId 
         , COUNT(DISTINCT Feeder.MPFeederId) FeederCount
@@ -114,8 +99,8 @@ CREATE PROCEDURE spSKerman_part1
         ) FaultyFeeder ON  ta.AreaId = FaultyFeeder.AreaId
     LEFT JOIN(
         SELECT R.AreaId
-        ,60 * 24 * @Days * SUM(CASE WHEN R.IsTamir = 1 THEN R.DisconnectPower END) / SUM(Sub.Energy) AS DisPowTamir
-        ,60 * 24 * @Days * SUM(CASE WHEN R.IsTamir = 0 THEN R.DisconnectPower END) / SUM(Sub.Energy) AS DisPow
+        ,60 * 24 * @Days * CAST(SUM(CASE WHEN R.IsTamir = 1 THEN R.DisconnectPower END) AS FLOAT) / SUM(Sub.Energy) AS DisPowTamir
+        ,60 * 24 * @Days * CAST(SUM(CASE WHEN R.IsTamir = 0 THEN R.DisconnectPower END) AS FLOAT) / SUM(Sub.Energy) AS DisPow
         FROM TblRequest R
         INNER JOIN #tmp t ON R.RequestId = t.RequestId
         INNER JOIN TblSubscribers Sub ON Sub.AreaId = R.AreaId
