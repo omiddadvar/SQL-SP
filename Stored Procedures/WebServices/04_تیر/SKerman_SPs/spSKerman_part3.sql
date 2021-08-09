1c693cfecf321359a482bb1930849ab8a3caa945
Omid Dadvar <Dadvar@fanni.tazarv.com>
2021-08-09 08:13:50 +0430

    come on... :weary:

--- Stored Procedures/WebServices/04_تیر/SKerman_SPs/spSKerman_part3.sql
+++ Stored Procedures/WebServices/04_تیر/SKerman_SPs/spSKerman_part3.sql
@@ -3,22 +3,22 @@ GO
 CREATE PROCEDURE spSKerman_part3
   AS 
   BEGIN
-    SELECT ta.AreaId , ta.Area
-      ,ISNULL(TblCnt.CntPublic , 0) AS CntPublic 
-      ,ISNULL(TblCnt.CntPublicMP , 0) AS CntPublicMP 
-      ,ISNULL(TblCnt.CntLP , 0) AS CntLP
-      FROM Tbl_Area ta
-      LEFT JOIN (
-        SELECT MP.AreaId 
-          ,COUNT(DISTINCT MP.MPFeederId) + COUNT(DISTINCT CASE WHEN ISNULL(LP.OwnershipId , 2) = 2 THEN LP.LPFeederId END) AS CntPublic
-          ,COUNT(DISTINCT MP.MPFeederId) AS CntPublicMP
-          ,COUNT(DISTINCT LP.LPFeederId) AS CntLP
-          FROM Tbl_MPFeeder MP
-          INNER JOIN Tbl_LPFeeder LP ON MP.AreaId = LP.AreaId
-          WHERE ISNULL(MP.OwnershipId , 2) = 2
-          GROUP BY MP.AreaId
-      )TblCnt ON ta.AreaId = TblCnt.AreaId
-      WHERE ta.IsCenter = 0
+      select Tbl_Area.areaId, Area, ISNULL( t1.cntMPPublic,0) + ISNULL(t2.cntLPPublic,0) as CntPublic,
+      ISNULL(t1.cntMPPublic,0) as CntPublicMP, ISNULL(t3.cntLP,0) as CntLP
+      from Tbl_Area
+      left join (
+      select AreaId, COUNT(*) as cntMPPublic from Tbl_MPFeeder where IsActive = 1 and isnull(ownershipid,2) = 2 
+      group by AreaId ) as t1
+      on Tbl_Area.AreaId = t1.AreaId
+      left join (
+      select AreaId, COUNT(*) as cntLPPublic from Tbl_LPFeeder where IsActive = 1 and isnull(ownershipid,2) = 2
+      group by AreaId ) as t2
+      on Tbl_Area.AreaId = t2.AreaId
+      left join (
+		select AreaId, COUNT(*) as cntLP from Tbl_LPFeeder where IsActive = 1
+		group by AreaId) as t3
+      on Tbl_Area.AreaId = t3.AreaId
+      where IsCenter = 0
   END
 
---   EXEC spsKerman_part3  
+--EXEC spsKerman_part3
