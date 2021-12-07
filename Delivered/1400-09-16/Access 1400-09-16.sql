2b20bb12682f4067a8b6da19859e6a51a81ebeb7
Omid Dadvar <omiddadvar94@gmail.com>
2021-12-07 17:10:31 +0330

    SQL update ...

---
+++ Delivered/1400-09-16/Access 1400-09-16.sql
@@ -0,0 +1,11 @@
+﻿IF NOT EXISTS (SELECT * FROM Tbl_FormObject WHERE FormObjectId = 201)
+            INSERT Tbl_FormObject (FormObjectId,Tag,PersianTag,SysObjId) VALUES (201,N'CanSendRepeatSMS',N'توانايي ارسال مجدد پيامک خاموشی به مشترکين',24)
+ELSE
+            UPDATE Tbl_FormObject SET Tag = N'CanSendRepeatSMS',PersianTag = N'توانايي ارسال مجدد پيامک خاموشی به مشترکين',SysObjId = 24 WHERE FormObjectId = 201
+GO
+
+IF NOT EXISTS (SELECT * FROM Tbl_FormObjectApplication WHERE FormObjectApplicationId = 292)
+            INSERT Tbl_FormObjectApplication (FormObjectApplicationId,ApplicationId,FormObjectId) VALUES (292,1,201)
+ELSE
+            UPDATE Tbl_FormObjectApplication SET ApplicationId = 1,FormObjectId = 201 WHERE FormObjectApplicationId = 292
+GO
--- Hints/ارسال پیامک مجدد اطلاع رسانی/Base Info.sql
+++ Hints/ارسال پیامک مجدد اطلاع رسانی/Base Info.sql
@@ -7,6 +7,6 @@ SELECT * FROM TblRequestInform WHERE RequestId = 9900000000001539 -- Delete
 
 SELECT * FROM Tbl_RequestInformJobState
 
-SELECT * FROM TblSubscriberInfom WHERE RequestInformId = 98900000098363 -- Delete
+SELECT * FROM TblSubscriberInfom WHERE RequestInformId = 98900000098363  -- If Exists -- Delete
 
 SELECT * FROM Tbl_SendSMSStatus 
