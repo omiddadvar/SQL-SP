c2b4121ae2850a2a88b0305c4c84182c880e4360
Omid Dadvar <omiddadvar94@gmail.com>
2021-12-13 12:39:44 +0330

    Some Homa SP needed change :weary:

---
+++ Stored Procedures/App/09_آذر/sp--Homa/sp 1400-09-23.sql
@@ -0,0 +1,576 @@
+ALTER PROCEDURE Homa.spTraceArea
+  @StartDate AS VARCHAR(10),
+  @EndDate AS VARCHAR(10),
+  @StartTime AS VARCHAR(8),
+  @EndTime AS VARCHAR(8),
+  @AreaId AS int = 0,
+  @TabletId AS BIGINT = -1
+  AS 
+  BEGIN
+    /*Calculation on time: */
+  	IF len(@StartTime) = 5
+		  SET @StartTime = @StartTime + ':00'
+  	IF len(@EndTime) = 5
+  		SET @EndTime = @EndTime + ':59'
+  	DECLARE @StartDT AS Datetime
+  	DECLARE @EndDT AS Datetime
+  	DECLARE @StartStr AS VARCHAR(13)
+  	DECLARE @EndStr AS VARCHAR(13)
+  	IF @StartDate = ''
+  	BEGIN 
+  		SET @StartDT = DATEADD(YEAR,-20,GETDATE())
+  		SET @StartStr = '1300/01/01 00'
+  	end
+  	ELSE
+  	BEGIN 
+  		SET @StartDT = dbo.shtom(@StartDate)
+  		SET @StartStr = @StartDate + ' 00'
+  		IF @StartTime <> ''
+  		BEGIN 
+  			SET @StartDT = DATEADD(HOUR,CAST(LEFT(@StartTime,2) AS int),@StartDT)
+  			SET @StartDT = DATEADD(MINUTE,CAST(SUBSTRING(@StartTime,4,2)AS int),@StartDT)
+  			SET @StartStr = @StartDate + ' ' + LEFT(@StartTime,2)
+  		END
+  	END
+  IF @EndDate = ''
+	BEGIN 
+		SET @EndDT = DATEADD(YEAR, + 20,GETDATE())
+		SET @EndStr = '1500/01/01 00'
+	END
+	ELSE
+	BEGIN 
+		SET @EndDT = dbo.shtom(@EndDate)
+		SET @EndStr = @EndDate + ' 23'
+		IF @EndTime <> ''
+		BEGIN 
+			SET @EndDT = DATEADD(HOUR,CAST(LEFT(@EndTime,2) AS int),@EndDT)
+			SET @EndDT = DATEADD(MINUTE,CAST(SUBSTRING(@EndTime,4,2)AS int),@EndDT)
+			SET @EndDT = DATEADD(SECOND,59,@EndDT)
+			SET @EndStr = @EndDate + ' ' + LEFT(@EndTime,2)
+		END
+	END
+  /*Gathering Data: */
+	SELECT
+		  [TblOnCall].[OnCallId],[TblTraceSummary].AreaId,TargetDatePersian + 
+		  case when TargetTime< = 9 THEN ' 0' 
+		  ELSE
+		  ' ' END + CAST(TargetTime AS VARCHAR(10))
+		  AS TargetDT,
+		  [TblTraceSummary].TraceLen		  
+		  INTO #tmpOnCall1
+		  FROM [Homa].[TblTraceSummary]
+		  INNER JOIN [Homa].[TblOnCall]
+		  ON [TblTraceSummary].OnCallId = [TblOnCall].OnCallId
+		  WHERE 
+		  (TargetDatePersian> = @StartDate or @StartDate = '')and
+		  (TargetDatePersian< = @EndDate or @EndDate = '')and
+		  (AreaId = @AreaId or @AreaId< = 0)
+  		  
+  	IF @StartTime <> '' 
+  		DELETE FROM #tmpOnCall1 WHERE TargetDT < @StartStr
+  	IF @EndTime <> '' 
+  		DELETE FROM #tmpOnCall1 WHERE TargetDT > @EndStr
+		SELECT #tmpOnCall1.OnCallId,#tmpOnCall1.AreaId,SUM(#tmpOnCall1.TraceLen) AS TraceLen INTO #tmpOnCall FROM #tmpOnCall1 
+	    GROUP BY #tmpOnCall1.OnCallId,#tmpOnCall1.AreaId
+    DROP TABLE #tmpOnCall1
+
+    /*Area Data :*/
+    DECLARE @lSQL VARCHAR(2000) = N'SELECT #tmpOnCall.AreaId , min(Tbl_Area.Area) AS Area 
+        , round(sum(#tmpOnCall.TraceLen)/1000,2) AS TraceLen
+        ,CAST(0 AS BIT) AS IsChecked
+      FROM #tmpOnCall
+      INNER JOIN homa.TblOnCall ON TblOnCall.OnCallId = #tmpOnCall.OnCallId
+      INNER JOIN Tbl_Area ON Tbl_Area.AreaId = #tmpOnCall.AreaId'
+    IF ISNULL(@TabletId , 0) > 0 BEGIN  
+      SET @lSQL = @lSQL +' WHERE TblOnCall.TabletId = ' + CAST(@TabletId AS VARCHAR(30)) 
+    END
+    SET @lSQL = @lSQL +' GROUP BY #tmpOnCall.AreaId'
+    /*Final Result*/
+    EXEC(@lSQL)
+
+    DROP TABLE #tmpOnCall
+  END
+GO
+
+/*-----------------------------------------------------------------------------------------------------------*/
+  ALTER PROCEDURE Homa.spTraceMaster 
+  @StartDate AS VARCHAR(10),
+  @EndDate AS VARCHAR(10),
+  @StartTime AS VARCHAR(8),
+  @EndTime AS VARCHAR(8),
+  @AreaId AS int = 0,
+  @TabletId AS BIGINT = -1
+  AS 
+  BEGIN
+    /*Calculation on time: */
+  	IF len(@StartTime) = 5
+		  SET @StartTime = @StartTime + ':00'
+  	IF len(@EndTime) = 5
+  		SET @EndTime = @EndTime + ':59'
+  	DECLARE @StartDT AS Datetime
+  	DECLARE @EndDT AS Datetime
+  	DECLARE @StartStr AS VARCHAR(13)
+  	DECLARE @EndStr AS VARCHAR(13)
+  	IF @StartDate = ''
+  	BEGIN 
+  		SET @StartDT = DATEADD(YEAR,-20,GETDATE())
+  		SET @StartStr = '1300/01/01 00'
+  	end
+  	ELSE
+  	BEGIN 
+  		SET @StartDT = dbo.shtom(@StartDate)
+  		SET @StartStr = @StartDate + ' 00'
+  		IF @StartTime <> ''
+  		BEGIN 
+  			SET @StartDT = DATEADD(HOUR,CAST(LEFT(@StartTime,2) AS int),@StartDT)
+  			SET @StartDT = DATEADD(MINUTE,CAST(SUBSTRING(@StartTime,4,2)AS int),@StartDT)
+  			SET @StartStr = @StartDate + ' ' + LEFT(@StartTime,2)
+  		END
+  	END
+  IF @EndDate = ''
+	BEGIN 
+		SET @EndDT = DATEADD(YEAR, + 20,GETDATE())
+		SET @EndStr = '1500/01/01 00'
+	END
+	ELSE
+	BEGIN 
+		SET @EndDT = dbo.shtom(@EndDate)
+		SET @EndStr = @EndDate + ' 23'
+		IF @EndTime <> ''
+		BEGIN 
+			SET @EndDT = DATEADD(HOUR,CAST(LEFT(@EndTime,2) AS int),@EndDT)
+			SET @EndDT = DATEADD(MINUTE,CAST(SUBSTRING(@EndTime,4,2)AS int),@EndDT)
+			SET @EndDT = DATEADD(SECOND,59,@EndDT)
+			SET @EndStr = @EndDate + ' ' + LEFT(@EndTime,2)
+		END
+	END
+  /*Gathering Data: */
+	SELECT
+		  [TblOnCall].[OnCallId],[TblTraceSummary].AreaId,TargetDatePersian + 
+		  case when TargetTime< = 9 THEN ' 0' 
+		  ELSE
+		  ' ' END + CAST(TargetTime AS VARCHAR(10))
+		  AS TargetDT,
+		  [TblTraceSummary].TraceLen		  
+		  INTO #tmpOnCall1
+		  FROM [Homa].[TblTraceSummary]
+		  INNER JOIN [Homa].[TblOnCall]
+		  ON [TblTraceSummary].OnCallId = [TblOnCall].OnCallId
+		  WHERE 
+		  (TargetDatePersian> = @StartDate or @StartDate = '')and
+		  (TargetDatePersian< = @EndDate or @EndDate = '')and
+		  (AreaId = @AreaId or @AreaId< = 0)
+  		  
+  	IF @StartTime <> '' 
+  		DELETE FROM #tmpOnCall1 WHERE TargetDT < @StartStr
+  	IF @EndTime <> '' 
+  		DELETE FROM #tmpOnCall1 WHERE TargetDT > @EndStr
+		SELECT #tmpOnCall1.OnCallId,#tmpOnCall1.AreaId,SUM(#tmpOnCall1.TraceLen) AS TraceLen INTO #tmpOnCall FROM #tmpOnCall1 
+	    GROUP BY #tmpOnCall1.OnCallId,#tmpOnCall1.AreaId
+    DROP TABLE #tmpOnCall1
+
+    /*Master Data :*/
+    DECLARE @lSQL VARCHAR(2000) = N'SELECT TblOnCall.MasterId,min(Tbl_Master.Name) AS MasterName
+        ,round(sum(#tmpOnCall.TraceLen)/1000, 2) AS TraceLen
+        ,CAST(0 AS BIT) AS IsChecked
+      FROM #tmpOnCall
+      INNER JOIN homa.TblOnCall ON TblOnCall.OnCallId = #tmpOnCall.OnCallId
+      INNER JOIN Tbl_Master ON Tbl_Master.MasterId = TblOnCall.MasterId'
+    IF ISNULL(@TabletId , 0) > 0 BEGIN  
+      SET @lSQL = @lSQL +' WHERE TblOnCall.TabletId = ' + CAST(@TabletId AS VARCHAR(30))
+    END
+    SET @lSQL = @lSQL +' GROUP BY TblOnCall.MasterId'
+    /*Final Result*/
+    EXEC(@lSQL) 
+
+    DROP TABLE #tmpOnCall
+  END
+GO
+/*-----------------------------------------------------------------------------------------------------------*/
+  ALTER PROCEDURE Homa.spTraceOnCall
+   @StartDate as varchar(10),
+   @EndDate as varchar(10),
+   @StartTime as varchar(8),
+   @EndTime as varchar(8),
+   @AreaId as INT=0,
+   @Master as varchar(4000)='',
+   @Tablet as varchar(4000)='',
+   @Area as varchar(4000)=''
+  AS
+  BEGIN
+    /*Calculate Time: */
+  	if len(@StartTime)=5
+  		set @StartTime=@StartTime+':00'
+  	if len(@EndTime)=5
+  		set @EndTime=@EndTime+':59'
+  	declare @StartDT as Datetime
+  	declare @EndDT as Datetime
+  	declare @StartStr as varchar(13)
+  	declare @EndStr as varchar(13)
+  	if @StartDate=''
+  	begin
+  		set @StartDT=DATEADD(YEAR,-20,GETDATE())
+  		set @StartStr='1300/01/01 00'
+  	end
+  	else
+  	begin
+  		set @StartDT=dbo.shtom(@StartDate)
+  		set @StartStr=@StartDate + ' 00'
+  		if @StartTime <>''
+  		begin
+  			set @StartDT=DATEADD(HOUR,CAST(LEFT(@StartTime,2) as int),@StartDT)
+  			set @StartDT=DATEADD(MINUTE,CAST(SUBSTRING(@StartTime,4,2)as int),@StartDT)
+  			set @StartStr=@StartDate+ ' '+LEFT(@StartTime,2)
+  		end
+  	end
+  	if @EndDate=''
+  	begin
+  		set @EndDT=DATEADD(YEAR,+20,GETDATE())
+  		set @EndStr='1500/01/01 00'
+  	end
+  	else
+  	begin
+  		set @EndDT=dbo.shtom(@EndDate)
+  		set @EndStr=@EndDate + ' 23'
+  		if @EndTime <>''
+  		begin
+  			set @EndDT=DATEADD(HOUR,CAST(LEFT(@EndTime,2) as int),@EndDT)
+  			set @EndDT=DATEADD(MINUTE,CAST(SUBSTRING(@EndTime,4,2)as int),@EndDT)
+  			set @EndDT=DATEADD(SECOND,59,@EndDT)
+  			set @EndStr=@EndDate+ ' '+LEFT(@EndTime,2)
+  		end
+  	END
+    /*Gathering Data: */
+	  SELECT
+		  distinct [TblOnCall].[OnCallId],[TblTraceSummary].AreaId,TargetDatePersian+
+		  case when TargetTime<=9 then ' 0' 
+		  else
+		  ' ' end+ CAST(TargetTime AS varchar(10))
+		  as TargetDT,
+		  [TblTraceSummary].TraceLen
+		  into #tmpOnCall1
+		  FROM [Homa].[TblTraceSummary]
+		  inner join [Homa].[TblOnCall]
+		  on [TblTraceSummary].OnCallId=[TblOnCall].OnCallId
+		  where 
+		  (TargetDatePersian>=@StartDate or @StartDate='')and
+		  (TargetDatePersian<=@EndDate or @EndDate='')and
+		  (AreaId<=@AreaId or @AreaId<=0)
+		  
+  	if @StartTime<>'' 
+  		delete from #tmpOnCall1 where TargetDT <@StartStr
+  	if @EndTime<>'' 
+  		delete from #tmpOnCall1 where TargetDT >@EndStr
+  	
+  	select distinct OnCallId , AreaId into #tmpOnCall from #tmpOnCall1 
+  	  
+    drop table #tmpOnCall1
+    /*Check Master-Tablet-Area: */
+    if @Master<>'' 
+    	begin
+    		delete from #tmpOnCall
+    		where not OnCallId
+    		in (
+    			select #tmpOnCall.OnCallId from #tmpOnCall 
+    			inner join homa.TblOnCall on TblOnCall.OnCallId =#tmpOnCall.OnCallId
+    			where MasterId in (
+    			select item from dbo.Split(@Master,','))
+    		)
+    	end
+  	if @Tablet<>'' 
+    	begin
+    		delete from #tmpOnCall
+    		where not OnCallId
+    		in (
+    			select #tmpOnCall.OnCallId from #tmpOnCall 
+    			inner join homa.TblOnCall on TblOnCall.OnCallId =#tmpOnCall.OnCallId
+    			where TabletId in (
+    			select item from dbo.Split(@Tablet,','))
+    		)
+    	end
+  	if @Area<>'' 
+    	begin
+    		delete from #tmpOnCall
+    		where not OnCallId
+    		in (
+    			select #tmpOnCall.OnCallId from #tmpOnCall 
+    			where AreaId in (
+    			select item from dbo.Split(@Area,','))
+    		)
+    	end
+    
+    select distinct OnCallId from #tmpOnCall
+    	      
+    drop table #tmpOnCall
+  END
+GO
+/*-----------------------------------------------------------------------------------------------------------*/
+  ALTER PROCEDURE Homa.spTraceRequest
+  @StartDate as varchar(10),
+  @EndDate as varchar(10),
+  @StartTime as varchar(8),
+  @EndTime as varchar(8),
+  @AreaId as INT  =  0 ,
+  @Master as varchar(4000) = '',
+  @Tablet as varchar(4000) = '',
+  @Area as varchar(4000) = ''
+  AS
+  BEGIN
+    /*Calculate Time: */
+    if len(@StartTime) = 5
+  		set @StartTime = @StartTime + ':00'
+  	if len(@EndTime) = 5
+  		set @EndTime = @EndTime + ':59'
+  	declare @StartDT as Datetime
+  	declare @EndDT as Datetime
+  	declare @StartStr as varchar(13)
+  	declare @EndStr as varchar(13)
+  	if @StartDate = ''
+  	begin
+  		set @StartDT = DATEADD(YEAR,-20,GETDATE())
+  		set @StartStr = '1300/01/01 00'
+  	end
+  	else
+  	begin
+  		set @StartDT = dbo.shtom(@StartDate)
+  		set @StartStr = @StartDate  +  ' 00'
+  		if @StartTime <> ''
+  		begin
+  			set @StartDT = DATEADD(HOUR,CAST(LEFT(@StartTime,2) as int),@StartDT)
+  			set @StartDT = DATEADD(MINUTE,CAST(SUBSTRING(@StartTime,4,2)as int),@StartDT)
+  			set @StartStr = @StartDate +  ' ' + LEFT(@StartTime,2)
+  		end
+  	end
+  	if @EndDate = ''
+  	begin
+  		set @EndDT = DATEADD(YEAR, + 20,GETDATE())
+  		set @EndStr = '1500/01/01 00'
+  	end
+  	else
+  	begin
+  		set @EndDT = dbo.shtom(@EndDate)
+  		set @EndStr = @EndDate  +  ' 23'
+  		if @EndTime <> ''
+  		begin
+  			set @EndDT = DATEADD(HOUR,CAST(LEFT(@EndTime,2) as int),@EndDT)
+  			set @EndDT = DATEADD(MINUTE,CAST(SUBSTRING(@EndTime,4,2)as int),@EndDT)
+  			set @EndDT = DATEADD(SECOND,59,@EndDT)
+  			set @EndStr = @EndDate +  ' ' + LEFT(@EndTime,2)
+  		end
+  	END
+    /*Gather Data: */
+  	SELECT
+		  distinct [TblOnCall].[OnCallId],[TblTraceSummary].AreaId,TargetDatePersian+
+		  case when TargetTime<=9 then ' 0' 
+		  else
+		  ' ' end+ CAST(TargetTime AS varchar(10))
+		  as TargetDT,
+		  [TblTraceSummary].TraceLen
+		  
+		  
+		  into #tmpOnCall1
+		  FROM [Homa].[TblTraceSummary]
+		  inner join [Homa].[TblOnCall]
+		  on [TblTraceSummary].OnCallId=[TblOnCall].OnCallId
+		  where 
+		  (TargetDatePersian>=@StartDate or @StartDate='')and
+		  (TargetDatePersian<=@EndDate or @EndDate='')and
+		  (AreaId<=@AreaId or @AreaId<=0)
+		  
+  	if @StartTime<>'' 
+  		delete from #tmpOnCall1 where TargetDT <@StartStr
+  	if @EndTime<>'' 
+  		delete from #tmpOnCall1 where TargetDT >@EndStr
+  	
+  	select distinct #tmpOnCall1.OnCallId,#tmpOnCall1.AreaId into #tmpOnCall from #tmpOnCall1 
+  	drop table #tmpOnCall1
+    /*Check Master-Tablet-Area: */
+  	if @Master<>'' 
+  	begin
+  		delete from #tmpOnCall
+  		where not OnCallId
+  		in (
+  			select #tmpOnCall.OnCallId from #tmpOnCall 
+  			inner join homa.TblOnCall on TblOnCall.OnCallId =#tmpOnCall.OnCallId
+  			where MasterId in (
+  			select item from dbo.Split(@Master,','))
+  		)
+  	end
+  	if @Tablet<>'' 
+  	begin
+  		delete from #tmpOnCall
+  		where not OnCallId
+  		in (
+  			select #tmpOnCall.OnCallId from #tmpOnCall 
+  			inner join homa.TblOnCall on TblOnCall.OnCallId =#tmpOnCall.OnCallId
+  			where TabletId in (
+  			select item from dbo.Split(@Tablet,','))
+  		)
+  	end
+  	if @Area<>'' 
+  	begin
+  		delete from #tmpOnCall
+  		where not OnCallId
+  		in (
+  			select #tmpOnCall.OnCallId from #tmpOnCall 
+  			where AreaId in (
+  			select item from dbo.Split(@Area,','))
+  		)
+  	END 
+
+    select * into #tmpEkipArrive from Homa.TblEkipArrive 
+      where OnCallId in (
+        SELECT
+      		 [OnCallId]	      
+      	  FROM #tmpOnCall
+      )
+    select * into #tmpStartMove from Homa.TblEkipStartMove
+      where OnCallId in (
+    	  SELECT
+    		 [OnCallId]	      
+    	  FROM #tmpOnCall
+      )
+    select TblJob.JobId, TblRequest.ConnectDT 
+      into #tmpJob
+      from Homa.TblJob 
+      inner join TblRequest on TblJob.RequestId=TblRequest.RequestId
+      where OncallId
+       in (
+    	  SELECT
+    		 [OnCallId]	      
+    	  FROM #tmpOnCall
+      )
+    select #tmpStartMove.OnCallId,#tmpStartMove.JobId, #tmpStartMove.StartMoveDT,#tmpEkipArrive.ArriveDT  ,#tmpJob.ConnectDT
+      into #tmpFind
+      from #tmpStartMove 
+    	inner join #tmpJob on #tmpJob.JobId=#tmpStartMove.JobId
+    	
+    	inner join #tmpEkipArrive on #tmpStartMove.OnCallId=#tmpEkipArrive.OnCallId
+    	and #tmpStartMove.JobId=#tmpEkipArrive.JobId
+    	where	 
+    	(		
+    		#tmpJob.ConnectDT >=@StartDT 
+    		and 
+    		#tmpJob.ConnectDT <=@EndDT
+    	)or
+    	(
+    		#tmpStartMove.StartMoveDT>=@StartDT
+    		and 
+    		#tmpStartMove.StartMoveDT<=@EndDT
+    	)
+    	or
+    	(
+    		#tmpStartMove.StartMoveDT<=@StartDT
+    		and 
+    		#tmpJob.ConnectDT>=@StartDT
+    	)
+    SELECT #tmpFind.OnCallId,#tmpFind.JobId,TblOnCall.TabletId,TblOnCall.MasterId,#tmpOnCall.AreaId,TblJob.RequestId,R.RequestNumber,
+      R.Address , R.Telephone , R.DisconnectDatePersian , R.DisconnectTime,
+    	R.ConnectDatePersian, R.ConnectTime , CAST(0 AS BIT) AS IsChecked
+      FROM #tmpFind
+      inner join homa.TblOnCall on TblOnCall.OnCallId=#tmpFind.OnCallId
+      inner join homa.TblJob on TblJob.JobId=#tmpFind.JobId
+      inner join TblRequest R on R.RequestId=TblJob.RequestId
+      inner join #tmpOnCall on #tmpOnCall.OnCallId=#tmpFind.OnCallId
+
+    drop table #tmpOnCall
+    drop table #tmpFind
+    drop table #tmpStartMove
+    drop table #tmpEkipArrive
+    drop table #tmpJob
+  END
+GO
+/*-----------------------------------------------------------------------------------------------------------*/
+  ALTER PROCEDURE Homa.spTraceTablet
+  @StartDate AS VARCHAR(10),
+  @EndDate AS VARCHAR(10),
+  @StartTime AS VARCHAR(8),
+  @EndTime AS VARCHAR(8),
+  @AreaId AS int = 0,
+  @TabletId AS BIGINT = -1
+  AS 
+  BEGIN
+    /*Calculation on time: */
+  	IF len(@StartTime) = 5
+		  SET @StartTime = @StartTime + ':00'
+  	IF len(@EndTime) = 5
+  		SET @EndTime = @EndTime + ':59'
+  	DECLARE @StartDT AS Datetime
+  	DECLARE @EndDT AS Datetime
+  	DECLARE @StartStr AS VARCHAR(13)
+  	DECLARE @EndStr AS VARCHAR(13)
+  	IF @StartDate = ''
+  	BEGIN 
+  		SET @StartDT = DATEADD(YEAR,-20,GETDATE())
+  		SET @StartStr = '1300/01/01 00'
+  	end
+  	ELSE
+  	BEGIN 
+  		SET @StartDT = dbo.shtom(@StartDate)
+  		SET @StartStr = @StartDate + ' 00'
+  		IF @StartTime <> ''
+  		BEGIN 
+  			SET @StartDT = DATEADD(HOUR,CAST(LEFT(@StartTime,2) AS int),@StartDT)
+  			SET @StartDT = DATEADD(MINUTE,CAST(SUBSTRING(@StartTime,4,2)AS int),@StartDT)
+  			SET @StartStr = @StartDate + ' ' + LEFT(@StartTime,2)
+  		END
+  	END
+  IF @EndDate = ''
+	BEGIN 
+		SET @EndDT = DATEADD(YEAR, + 20,GETDATE())
+		SET @EndStr = '1500/01/01 00'
+	END
+	ELSE
+	BEGIN 
+		SET @EndDT = dbo.shtom(@EndDate)
+		SET @EndStr = @EndDate + ' 23'
+		IF @EndTime <> ''
+		BEGIN 
+			SET @EndDT = DATEADD(HOUR,CAST(LEFT(@EndTime,2) AS int),@EndDT)
+			SET @EndDT = DATEADD(MINUTE,CAST(SUBSTRING(@EndTime,4,2)AS int),@EndDT)
+			SET @EndDT = DATEADD(SECOND,59,@EndDT)
+			SET @EndStr = @EndDate + ' ' + LEFT(@EndTime,2)
+		END
+	END
+  /*Gathering Data: */
+	SELECT
+		  [TblOnCall].[OnCallId],[TblTraceSummary].AreaId,TargetDatePersian + 
+		  case when TargetTime< = 9 THEN ' 0' 
+		  ELSE
+		  ' ' END + CAST(TargetTime AS VARCHAR(10))
+		  AS TargetDT,
+		  [TblTraceSummary].TraceLen		  
+		  INTO #tmpOnCall1
+		  FROM [Homa].[TblTraceSummary]
+		  INNER JOIN [Homa].[TblOnCall]
+		  ON [TblTraceSummary].OnCallId = [TblOnCall].OnCallId
+		  WHERE 
+		  (TargetDatePersian> = @StartDate or @StartDate = '')and
+		  (TargetDatePersian< = @EndDate or @EndDate = '')and
+		  (AreaId = @AreaId or @AreaId< = 0)
+  		  
+  	IF @StartTime <> '' 
+  		DELETE FROM #tmpOnCall1 WHERE TargetDT < @StartStr
+  	IF @EndTime <> '' 
+  		DELETE FROM #tmpOnCall1 WHERE TargetDT > @EndStr
+		SELECT #tmpOnCall1.OnCallId,#tmpOnCall1.AreaId,SUM(#tmpOnCall1.TraceLen) AS TraceLen INTO #tmpOnCall FROM #tmpOnCall1 
+	    GROUP BY #tmpOnCall1.OnCallId,#tmpOnCall1.AreaId
+    DROP TABLE #tmpOnCall1
+
+    /*Tablet Data :*/
+    DECLARE @lSQL VARCHAR(2000) = N'SELECT TblOnCall.TabletId,min(Tbl_Tablet.TabletName) AS TabletName
+        ,round(sum(#tmpOnCall.TraceLen)/1000,2) AS TraceLen,
+        CAST(0 AS BIT) AS IsChecked
+      FROM #tmpOnCall
+      INNER JOIN homa.TblOnCall ON TblOnCall.OnCallId = #tmpOnCall.OnCallId
+      INNER JOIN Homa.Tbl_Tablet ON Tbl_Tablet.TabletId = TblOnCall.TabletId'
+    IF ISNULL(@TabletId , 0) > 0 BEGIN  
+      SET @lSQL = @lSQL +' WHERE TblOnCall.TabletId = ' + CAST(@TabletId AS VARCHAR(30))
+    END
+    SET @lSQL = @lSQL +' GROUP BY TblOnCall.TabletId'
+    /*Final Result*/
+    EXEC(@lSQL)
+
+    DROP TABLE #tmpOnCall
+  END
+GO
---
+++ Stored Procedures/App/09_آذر/spGetReport_6_21_ALL/spGetReport_6_21.sql
@@ -0,0 +1,71 @@
+
+ALTER PROCEDURE spGetReport_6_21
+   @aAreaIds AS VARCHAR(100)
+  ,@aMPPost AS VARCHAR(100)
+  ,@aMPFeederIds AS VARCHAR(100) 
+  ,@aLPPostIds AS VARCHAR(100)
+AS 
+BEGIN
+    DECLARE @lSQL AS VARCHAR(5000)
+    DECLARE @lWhere AS VARCHAR(500)
+    SET @lSQL = 'SELECT LPP.LPPostId
+          ,Spc_Sec.SpecValue AS SType, Sec.SerialNumber AS SSerial, Sec.IsCutable AS SCutable
+          ,Sec.IsGround AS SGround ,Spc_AppSec.SpecValue AS SApp ,Spc_FacSec.SpecValue AS SFac
+          ,Spc_Mec.SpecValue AS SMech ,Spc_Sar.SpecValue AS SSar, Spc_Size.SpecValue AS SSize
+		  ,Dej.SerialNumber ,Spc_Type.SpecValue AS DType , Spc_FacD.SpecValue AS DFac ,Spc_ModD.SpecValue AS DMod
+          ,Spc_AppD.SpecValue AS DApp ,Spc_FCurr.SpecValue AS DFCurr ,Spc_RelehDC.SpecValue AS DRCycle
+          ,Dej.IsReleh ,Dej.IsTermometer ,Dej.Isfan ,Dej.IsRelehBokh ,Releh.RelehType
+		  ,Fuse.SerialNumber AS FSerial ,Spc_FacF.SpecValue AS FFac ,Spc_ModF.SpecValue AS FMod 
+          ,Spc_AppF.SpecValue AS FApp ,Spc_Curr.SpecValue AS FCurr
+          ,CASE WHEN FuseSecsunerId IS NOT NULL THEN ''”ò”ÌÊ‰— ›ÌÊ“œ«—'' ELSE NULL END AS FType
+        FROM Tbl_LPPost LPP
+        INNER JOIN Tbl_MPFeeder MPF ON LPP.MPFeederId = MPF.MPFeederId
+        INNER JOIN Tbl_MPPost MPP ON MPF.MPPostId = MPP.MPPostId
+        INNER JOIN Tbl_Area A ON LPP.AreaId = A.AreaId
+        LEFT JOIN TblFuseSecsuner Fuse ON LPP.LPPostId = Fuse.LPPostId
+		LEFT JOIN TblSecsuner Sec ON LPP.LPPostId = Sec.LPPostId
+		LEFT JOIN TblDejenctor Dej ON LPP.LPPostId = Dej.LPPostId
+        INNER JOIN Tbl_LPPostInfo Info ON LPP.LPPostId = Info.LPPostId
+        LEFT JOIN TblSpec SpcB_Type ON Info.spcBarghGir_JensId = SpcB_Type.SpecId
+        LEFT JOIN TblSpec SpcB_Fac ON Info.spcBarghGir_FactoryId = SpcB_Fac.SpecId
+        LEFT JOIN TblSpec SpcB_Cur ON Info.spcBarghGir_CurrentId = SpcB_Cur.SpecId
+        LEFT JOIN TblSpec SpcC_Fuse ON Info.spcCutout_FuseCurrentId = SpcC_Fuse.SpecId
+        LEFT JOIN TblSpec SpcC_Fac ON Info.spcCutout_FactoryId = SpcC_Fac.SpecId
+        LEFT JOIN TblSpec SpcC_Kind ON Info.spcCutout_TypeId = SpcC_Kind.SpecId
+        LEFT JOIN TblSpec Spc_FacF ON Fuse.spcFactoryId = Spc_FacF.SpecId
+        LEFT JOIN TblSpec Spc_ModF ON Fuse.spcModelId = Spc_ModF.SpecId
+        LEFT JOIN TblSpec Spc_AppF ON Fuse.spcApplicationId = Spc_AppF.SpecId
+        LEFT JOIN TblSpec Spc_Curr ON Fuse.spcFuseCurrentId = Spc_Curr.SpecId 
+        LEFT JOIN TblSpec Spc_Type ON Dej.spcDejenctortypeId = Spc_Type.SpecId
+        LEFT JOIN TblSpec Spc_FacD ON Dej.spcDejenctor_FactoryId = Spc_FacD.SpecId
+        LEFT JOIN TblSpec Spc_ModD ON Dej.spcModelId = Spc_ModD.SpecId
+        LEFT JOIN TblSpec Spc_AppD ON Dej.spcApplicationId = Spc_AppD.SpecId
+        LEFT JOIN TblSpec Spc_FCurr ON Dej.spcFCurrentId = Spc_FCurr.SpecId
+        LEFT JOIN TblSpec Spc_RelehDC ON Dej.spcRelehDCCycleType = Spc_RelehDC.SpecId
+        LEFT JOIN Tbl_RelehType Releh ON Dej.RelehTypeId = Releh.RelehTypeId 
+		LEFT JOIN TblSpec Spc_Sec ON Sec.spcSecsunerTypeId = Spc_Sec.SpecId
+        LEFT JOIN TblSpec Spc_AppSec ON Sec.spcApplicationId = Spc_AppSec.SpecId
+        LEFT JOIN TblSpec Spc_FacSec ON Sec.spcFactoryId = Spc_FacSec.SpecId
+        LEFT JOIN TblSpec Spc_Mec ON Sec.spcMechanismId = Spc_Mec.SpecId
+        LEFT JOIN TblSpec Spc_Sar ON Sec.spcSarkablId = Spc_Sar.SpecId
+        LEFT JOIN TblSpec Spc_Size ON Sec.spcCableSizeId = Spc_Size.SpecId '
+
+      SET @lWhere = ' WHERE LPP.IsHavayi = 0 AND (not Fuse.FuseSecsunerId is null or not Sec.SecsunerId is null or not Dej.DejenctorId is null) '
+      IF @aLPPostIds <> '' BEGIN
+        SET @lWhere = @lWhere + ' AND LPP.LPPostId IN (' + @aLPPostIds + ')'
+      END
+      ELSE IF @aMPFeederIds <> '' BEGIN
+        SET @lWhere = @lWhere + ' AND MPF.MPFeederId IN (' + @aMPFeederIds + ')' 
+      END
+      ELSE IF @aMPPost <> '' BEGIN
+        SET @lWhere = @lWhere + ' AND MPP.MPPostId IN (' + @aMPPost + ')' 
+      END
+      ELSE IF @aAreaIds <> '' BEGIN
+      	SET @lWhere = @lWhere + ' AND A.AreaId IN (' + @aAreaIds + ')'
+      END
+      SET @lSQL = @lSQL + @lWhere
+      
+      EXEC(@lSQL)
+--      PRINT(@lSQL)
+END 
+GO
