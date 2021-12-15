d7d90f882b284b6a0ab3a6142332247e7850dec0
Omid Dadvar <omiddadvar94@gmail.com>
2021-12-15 14:59:04 +0330

    Prepare SMS For Ilam :weary:

--- Stored Procedures/App/09_آذر/spPrepareNotiMessage/spPrepareNotiMessage.sql
+++ Stored Procedures/App/09_آذر/spPrepareNotiMessage/spPrepareNotiMessage.sql
@@ -1,5 +1,4 @@
---USE CCRequesterSetad
---GO
+
 IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'[dbo].spPrepareNotiMessage') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
   DROP PROCEDURE [dbo].spPrepareNotiMessage
 GO
@@ -7,7 +6,8 @@ CREATE PROCEDURE [dbo].spPrepareNotiMessage @aReqId BIGINT
 AS
   BEGIN
     DECLARE @lText AS NVARCHAR(MAX) = ''
-      ,@lSubject AS NVARCHAR(1000) = 'DEFAULT VALUE'
+      ,@lRaw AS NVARCHAR(MAX) = ''
+      ,@lSubject AS NVARCHAR(1000) = 'Default-Value'
       ,@lDuration AS INT
       ,@lDisconnectDatePersian AS VARCHAR(10)
       ,@lDisconnectTime AS VARCHAR(5)
@@ -21,7 +21,7 @@ AS
     SELECT * INTO #tmp FROM TblRequest WHERE RequestId = @aReqId
     
     SET @lIsOk = CAST(CASE WHEN (SELECT COUNT(*) FROM #tmp) > 0 THEN 1 ELSE 0 END AS BIT)
-
+    
     SELECT @lDuration =  DATEDIFF(MINUTE, TamirDisconnectFromDT , TamirDisconnectToDT)
       , @lDisconnectDatePersian = DisconnectDatePersian 
       , @lDisconnectTime = DisconnectTime
@@ -34,19 +34,21 @@ AS
       INNER JOIN Tbl_TamirRequestSubject Sub ON TR.TamirRequestSubjectId = Sub.TamirRequestSubjectId
       WHERE R.RequestId = @aReqId
     SET @lText = REPLACE(@lText , 'Ì' , 'Ì')
-    IF CHARINDEX('„œÌ—Ì  «÷ÿ—«—Ì »«—',@lText) > 0 BEGIN
-      	 SET @lText = REPLACE(@lText , 'ÃÂ  »Â»Êœ ‰Ì—Ê —”«‰Ì' , '«Õ „«·«')
-      END
+    SET @lSubject = REPLACE(@lSubject , 'Ì' , 'Ì')
+    SET @lText = REPLACE(@lText , 'Probably' , CASE WHEN CHARINDEX('„œÌ—Ì  «÷ÿ—«—Ì »«—', @lSubject) > 0 THEN N'«Õ „«·«' ELSE '' END)
     SET @lText = REPLACE(@lText , 'DisconnectDatePersian' , @lDisconnectDatePersian)
     SET @lText = REPLACE(@lText , 'DisconnectTime' , @lDisconnectTime)
-    SET @lText = REPLACE(@lText , 'ss' , CAST(@lDuration AS VARCHAR(4)))
+    SET @lText = REPLACE(@lText , 'SS' , CAST(@lDuration AS VARCHAR(4)))
     SET @lText = REPLACE(@lText , 'Subject' , @lSubject)
     
     SELECT @lIsOk AS IsOk, @lSubject AS Subject, @lText AS Text
     DROP TABLE #tmp
   END
+GO
+/*
+IF NOT EXISTS (SELECT * FROM Tbl_Config WHERE ConfigId = 84)
+INSERT Tbl_Config (ConfigId,ConfigName,ConfigValue,ConfigText) VALUES (84, N' „‘ —ò ê—«„Ì »—ﬁ ‘„« »Â ⁄·  Subject° ÃÂ  »Â»Êœ ‰Ì—Ê —”«‰Ì Probably œ—  «—ÌŒ DisconnectDatePersian «“ ”«⁄  DisconnectTime »„œ   ﬁ—Ì»Ì ss œﬁÌﬁÂ ﬁÿ⁄ ŒÊ«Âœ ‘œ.  Ê“Ì⁄ »—ﬁ')
+GO
+*/
 
---IF NOT EXISTS (SELECT * FROM Tbl_Config WHERE ConfigId = 84)
---INSERT Tbl_Config (ConfigId,ConfigName,ConfigValue,ConfigText) VALUES (84,N'NotifBaBarname',NULL,N'„‘ —ò ê—«„Ì »—ﬁ ‘„« »Â ⁄·  Subject° «Õ „«·« œ—  «—ÌŒ DisconnectDatePersian «“ ”«⁄  DisconnectTime »„œ   ﬁ—Ì»Ì ss œﬁÌﬁÂ ﬁÿ⁄ ŒÊ«Âœ ‘œ. 
--- Ê“Ì⁄ »—ﬁ ')
---GO
+
