9e75f97cbc5bf237d7133debb1aba94491865aaf
Omid Dadvar <omiddadvar94@gmail.com>
2022-04-06 16:56:02 +0430

    Ilam Scripts Are Ready Bitch :stuck_out_tongue_winking_eye:

--- Fucking SQLs/ایلام/1401-02-01/Ilam_Holeilan_Script/Script_Edit.sql
+++ Fucking SQLs/ایلام/1401-02-01/Ilam_Holeilan_Script/Script_Edit.sql
@@ -2,6 +2,32 @@ CREATE TABLE #tmp
 (
 	RequestId BIGINT UNIQUE,
   AreaId INT,
+  IsMP BIT,
   Type VARCHAR(3) NULL
 )
+
+DECLARE @AreaId AS INT = 12
+
+UPDATE TblRequest
+  SET AreaId = @AreaId
+  FROM
+    TblRequest R
+    INNER JOIN #tmp T ON R.RequestId = T.RequestId
+
+UPDATE TblMPRequest
+  SET AreaId = @AreaId
+  FROM
+    TblMPRequest MPR
+    INNER JOIN TblRequest R ON MPR.MPRequestId = R.MPRequestId
+    INNER JOIN #tmp T ON R.RequestId = T.RequestId
+  WHERE T.IsMP = 1
+
+  
+UPDATE TblLPRequest
+  SET AreaId = @AreaId
+  FROM
+    TblLPRequest LPR
+    INNER JOIN TblRequest R ON LPR.LPRequestId = R.LPRequestId
+    INNER JOIN #tmp T ON R.RequestId = T.RequestId
+  WHERE T.IsMP = 0
 
--- Fucking SQLs/ایلام/1401-02-01/Ilam_Holeilan_Script/Script_Find.sql
+++ Fucking SQLs/ایلام/1401-02-01/Ilam_Holeilan_Script/Script_Find.sql
@@ -1,36 +1,40 @@
 
-DECLARE @AreaId AS INT = 10
+--DROP Table __Tbl_Holeilan
+
+--SELECT * FROM __Tbl_Holeilan ORDER BY type
+
+DECLARE @AreaId AS INT = 12
+
+SELECT * INTO __Tbl_Holeilan From (
 /*----------------MP Request-----------------*/
-SELECT TOP(100) R.RequestId
-    , CASE WHEN R.IsDisconnectMPFeeder = 1 THEN MPF.AreaId
-           WHEN MPR.IsTotalLPPostDisconnected = 1 THEN LPP.AreaId
-           ELSE  NULL END AS AreaId
-    , CASE WHEN R.IsDisconnectMPFeeder = 1 THEN 'MPF'
-           WHEN MPR.IsTotalLPPostDisconnected = 1 THEN 'LPP'
-           ELSE  NULL END AS Type
-  FROM TblRequest R
-  INNER JOIN TblMPRequest MPR ON R.MPRequestId = MPR.MPRequestId
-  LEFT JOIN Tbl_MPFeeder MPF ON MPR.MPFeederId = MPF.MPFeederId
-  LEFT JOIN Tbl_LPPost LPP ON MPR.LPPostId = LPP.LPPostId
-  WHERE R.IsMPRequest = 1 AND R.MPRequestId IS NOT NULL
-    AND (R.IsDisconnectMPFeeder = 1 OR MPR.IsTotalLPPostDisconnected = 1)
-    AND ISNULL(MPF.AreaId , @AreaId) = @AreaId 
-    AND ISNULL(LPP.AreaId , @AreaId) = @AreaId
+  SELECT R.RequestId, R.AreaId , CAST(1 AS BIT) IsMP
+      , CASE WHEN R.IsDisconnectMPFeeder = 1 THEN 'MPF'
+             WHEN MPR.IsTotalLPPostDisconnected = 1 THEN 'LPP'
+             ELSE  NULL END AS Type
+    FROM 
+      TblRequest R
+      INNER JOIN TblMPRequest MPR ON R.MPRequestId = MPR.MPRequestId
+      LEFT JOIN Tbl_MPFeeder MPF ON MPR.MPFeederId = MPF.MPFeederId
+      LEFT JOIN Tbl_LPPost LPP ON MPR.LPPostId = LPP.LPPostId
+    WHERE R.AreaId <> @AreaId 
+      AND (
+        (R.IsDisconnectMPFeeder = 1 AND MPF.AreaId = @AreaId) OR
+        (MPR.IsTotalLPPostDisconnected = 1 AND LPP.AreaId = @AreaId) 
+      ) 
 
-
+UNION
 /*----------------LP Request-----------------*/
-SELECT  TOP(100) R.RequestId
-    , CASE WHEN LPR.IsTotalLPPostDisconnected = 1 THEN LPP.AreaId
-           WHEN LPR.IsTotalLPFeederDisconnected = 1 THEN LPF.AreaId
-           ELSE  NULL END AS AreaId
-    , CASE WHEN LPR.IsTotalLPPostDisconnected = 1 THEN 'LPP'
-           WHEN LPR.IsTotalLPFeederDisconnected = 1 THEN 'LPF'
-           ELSE  NULL END AS Type
-   FROM TblRequest R
-  INNER JOIN TblLPRequest LPR ON R.LPRequestId = LPR.LPRequestId
-  LEFT JOIN Tbl_LPPost LPP ON LPR.LPPostId = LPP.LPPostId
-  LEFT JOIN Tbl_LPFeeder LPF ON LPR.LPFeederId = LPF.LPFeederId
-  WHERE R.IsLPRequest = 1 AND R.LPRequestId IS NOT NULL
-    AND (LPR.IsTotalLPPostDisconnected = 1 OR LPR.IsTotalLPFeederDisconnected = 1)
-    AND ISNULL(LPP.AreaId , @AreaId) = @AreaId
-    AND ISNULL(LPF.AreaId , @AreaId) = @AreaId 
+  SELECT R.RequestId, R.AreaId ,CAST(0 AS BIT) IsMP
+      , CASE WHEN LPR.IsTotalLPPostDisconnected = 1 THEN 'LPP'
+             ELSE 'LPF' END AS Type
+    FROM 
+      TblRequest R
+      INNER JOIN TblLPRequest LPR ON R.LPRequestId = LPR.LPRequestId
+      LEFT JOIN Tbl_LPPost LPP ON LPR.LPPostId = LPP.LPPostId
+      LEFT JOIN Tbl_LPFeeder LPF ON LPR.LPFeederId = LPF.LPFeederId
+    WHERE R.AreaId <> @AreaId
+        AND (
+        (LPR.IsTotalLPPostDisconnected = 1 AND LPP.AreaId = @AreaId) OR
+        (ISNULL(LPR.IsTotalLPPostDisconnected ,0) = 0 AND LPF.AreaId = @AreaId) 
+      ) 
+  ) tA
