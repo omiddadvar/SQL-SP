fc0a49e69687ac5881366a8c023f3a9c510688f0
Omid Dadvar <omiddadvar94@gmail.com>
2022-04-17 17:12:44 +0430

    Home sweet home :grin:

---
+++ Fucking SQLs/چهارمحال/1401-01-28/CH_SQL.sql
@@ -0,0 +1,26 @@
+﻿select top 10 TamirOperationListId from TblTamirOperationList
+where 
+	TamirOperationListId < 9900000000000000
+order by TamirOperationListId desc
+
+Select max(id) from _tblTamirRequestDetail
+
+/*
+SET IDENTITY_INSERT _tblTamirRequestDetail ON
+GO
+INSERT INTO _tblTamirRequestDetail (Id) VALUES (1013) 
+GO
+SET IDENTITY_INSERT _tblTamirRequestDetail OFF
+GO
+*/
+
+select top 10 * from TblTamirOperationList order by TamirOperationListId DESC
+
+---------------------------------------------------------------------------------------------
+
+  select count(*) from Tbl_EventLogCenter where WorkingAreaId = 10 and TableName IN (
+		'TblTamirRequest'
+		,'TblTamirRequestDisconnect'
+		)
+
+select * from TblDepartmentInfo
