ad4686a4bb4ae621999aa5bf0b6e8dc869a430b8
Omid Dadvar <omiddadvar94@gmail.com>
2022-05-09 17:25:31 +0430

    Ah, Shit :weary:

--- Temp/TEMP_Checker.sql
+++ Temp/TEMP_Checker.sql
@@ -1,7 +1,40 @@
-﻿SELECT * FROM Emergency.TblTiming 
+﻿
+SELECT * FROM Emergency.TblTiming WHERE GroupMPFeederId = 990188916
 
-SELECT * FROM Emergency.TblTimingMPFeeder  WHERE TimingId = 990188897
-
+SELECT * FROM Emergency.TblTimingMPFeeder  WHERE TimingId = 990188926
 
 SELECT * FROM Emergency.Tbl_TimingState 
 
+
+SELECT * FROM Emergency.Tbl_GroupMPFeeder
+
+
+SELECT * FROM Tbl_MPFeeder WHERE MPFeederId = 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+SELECT * FROM Tbl_ServiceLogApplication 
+SELECT * FROM Tbl_ServiceLogKeyType 
+SELECT * FROM Tbl_ServiceLogType
+SELECT * FROM Tbl_ServiceLogLevel
+
+SELECT * FROM TblServiceLog 
+
+
+SELECT * FROM ViewServiceLog 
+*/
