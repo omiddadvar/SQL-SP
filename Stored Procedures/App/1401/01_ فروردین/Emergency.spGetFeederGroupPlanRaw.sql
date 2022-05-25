4209bbe582148b0d926affd3a61dfe7e8b1328b5
Omid Dadvar <omiddadvar94@gmail.com>
2022-05-25 17:30:10 +0430

    Weekend :partying_face:

--- Stored Procedures/App/1401/01_ فروردین/Emergency.spGetFeederGroupPlanRaw.sql
+++ Stored Procedures/App/1401/01_ فروردین/Emergency.spGetFeederGroupPlanRaw.sql
@@ -1,58 +1,45 @@
 ﻿
-ALTER PROC Emergency.spGetFeederGroupPlanRaw @aGroupMPFeederId AS BIGINT
+ALTER PROC [Emergency].[spGetFeederGroupPlanRaw] @aGroupMPFeederId AS BIGINT
 AS
 BEGIN
 	SELECT MPT.MPFeederTemplateId
 		,MPF.MPFeederId
-    ,MPP.MPPostName
+		,MPP.MPPostName
 		,MPF.MPFeederName
-		,COUNT(R.MPRequestId) AS MPFeederDisconnectCount
+		,[Emergency].[GetMPFeederDisconnectCount](MPF.MPFeederId, MPT.IsDisconnectMPFeeder, MPT.MPFeederKeyId1) AS MPFeederDisconnectCount
 		,MPF.Voltage
-    ,A.Area
+		,A.Area
 	INTO #tmp
-	  FROM Emergency.Tbl_MPFeederTemplate MPT
+	FROM Emergency.Tbl_MPFeederTemplate MPT
 	INNER JOIN Tbl_MPFeeder MPF ON MPT.MPFeederId = MPF.MPFeederId
-  INNER JOIN Tbl_MPPost MPP ON MPF.MPPostId = MPP.MPPostId
-  INNER JOIN Tbl_Area A ON MPT.AreaId = A.AreaId
-	LEFT JOIN TblMPRequest MP ON MPF.MPFeederId = MP.MPFeederId
-	LEFT JOIN TblRequest R ON MP.MPRequestId = R.MPRequestId
-		AND DATEDIFF(DAY, R.DisconnectDT, GETDATE()) <= 20
-	WHERE ISNULL(R.IsDisconnectMPFeeder, 1) = 1
-		AND MPT.GroupMPFeederId = @aGroupMPFeederId
-	GROUP BY MPT.MPFeederTemplateId
-		,MPF.MPFeederId
-		,MPF.MPFeederName
-		,MPF.Voltage
-    ,MPP.MPPostName
-    ,A.Area
-
+	INNER JOIN Tbl_MPPost MPP ON MPF.MPPostId = MPP.MPPostId
+	INNER JOIN Tbl_Area A ON MPT.AreaId = A.AreaId
+	WHERE GroupMPFeederId = @aGroupMPFeederId
 
 	SELECT T.MPFeederTemplateId
-    ,T.Area
+		,T.Area
 		,T.MPFeederId
-    ,T.MPPostName
+		,T.MPPostName
 		,T.MPFeederName
 		,T.MPFeederDisconnectCount
-		,ISNULL(ROUND(3 * T.Voltage * C.CurrentValue * C.CosinPhi / 1000000, 2),0) AS CurrentValueMW
+		,ISNULL(ROUND(3 * T.Voltage * C.CurrentValue * C.CosinPhi / 1000000, 2), 0) AS CurrentValueMW
 		,ISNULL(C.CurrentValue, 0) AS CurrentValue
-    ,CAST(0 AS BIT) AS IsSelected
+		,CAST(0 AS BIT) AS IsSelected
 	FROM #tmp T
-    LEFT JOIN (
-  		SELECT L.MPFeederId
-  			,H.CurrentValue
-  			,H.CosinPhi
-  			,H.HourId
-  			,row_number() OVER (
-  				PARTITION BY L.MPFeederId ORDER BY L.RelDate
-  					,H.HourId DESC
-  				) AS RowNum
-  		FROM Tbl_MPFeederLoad L
-  		INNER JOIN Tbl_MPFeederLoadHours H ON L.MPFeederLoadId = H.MPFeederLoadId
-  		) C ON C.MPFeederId = T.MPFeederId
-	WHERE ISNULL(C.RowNum , 1) = 1
+	LEFT JOIN (
+		SELECT L.MPFeederId
+			,H.CurrentValue
+			,H.CosinPhi
+			,H.HourId
+			,row_number() OVER (
+				PARTITION BY L.MPFeederId ORDER BY L.RelDate
+					,H.HourId DESC
+				) AS RowNum
+		FROM Tbl_MPFeederLoad L
+		INNER JOIN Tbl_MPFeederLoadHours H ON L.MPFeederLoadId = H.MPFeederLoadId
+		) C ON C.MPFeederId = T.MPFeederId
+	WHERE ISNULL(C.RowNum, 1) = 1
+	ORDER BY T.MPFeederDisconnectCount
 
 	DROP TABLE #tmp
 END
-
-
--- EXEC Emergency.spGetFeederGroupPlan @aGroupMPFeederId = 990188852
---
+++ Stored Procedures/App/1401/02_اردیبهشت/[Function] Emergency.GetMPFeederDisconnectCount.sql
@@ -0,0 +1,41 @@
+
+ALTER FUNCTION [Emergency].[GetMPFeederDisconnectCount] (
+	@MPFeederId AS INT
+	,@IsDisconnectMPFeeder AS BIT
+	,@MPFeederKeyId AS BIGINT
+	)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @CntMP1 AS INT = 0
+	DECLARE @CntMP2 AS INT = 0
+	DECLARE @CntFT AS INT = 0
+
+	SELECT @CntMP1 = COUNT(*)
+	FROM TblMPRequest
+	INNER JOIN TblRequest ON TblMPRequest.MPRequestId = TblRequest.MPRequestId
+	WHERE TblMPRequest.DisconnectDT >= DATEADD(day, - 20, getdate())
+		AND ISNULL(TblRequest.IsDisconnectMPFeeder, 1) = 1
+		AND TblMPRequest.MPFeederId = @MPFeederId
+
+	IF @IsDisconnectMPFeeder = 0
+	BEGIN
+		SELECT @CntMP2 = COUNT(*)
+		FROM TblMPRequest
+		INNER JOIN TblRequest ON TblMPRequest.MPRequestId = TblRequest.MPRequestId
+		INNER JOIN TblMPRequestKey ON TblMPRequest.MPRequestId = TblMPRequestKey.MPRequestId
+		WHERE TblMPRequest.DisconnectDT >= DATEADD(day, - 20, getdate())
+			AND ISNULL(TblRequest.IsDisconnectMPFeeder, 1) = 0
+			AND TblMPRequest.MPFeederId = @MPFeederId
+			AND TblMPRequestKey.MPFeederKeyId = @MPFeederKeyId
+	END
+
+	SELECT @CntFT = COUNT(*)
+	FROM dbo.TblFogheToziDisconnect
+	INNER JOIN TblRequest ON TblRequest.FogheToziDisconnectId = TblFogheToziDisconnect.FogheToziDisconnectId
+	INNER JOIN dbo.TblFogheToziDisconnectMPFeeder ON TblFogheToziDisconnect.FogheToziDisconnectId = TblFogheToziDisconnectMPFeeder.FogheToziDisconnectId
+		AND TblRequest.DisconnectDT >= DATEADD(day, - 20, getdate())
+		AND TblFogheToziDisconnectMPFeeder.MPFeederId = @MPFeederId
+
+	RETURN isnull(@CntFT, 0) + isnull(@CntMP1, 0) + isnull(@CntMP2, 0)
+END
