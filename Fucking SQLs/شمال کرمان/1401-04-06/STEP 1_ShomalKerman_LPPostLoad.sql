11de5b969be4178b9d826e5aef64a4e1194d847c
Omid Dadvar <omiddadvar94@gmail.com>
2022-06-27 11:48:31 +0430

    ShKerman Script LPPostLoad

---
+++ Fucking SQLs/شمال کرمان/1401-04-06/STEP 1_ShomalKerman_LPPostLoad.sql
@@ -0,0 +1,68 @@
+﻿
+---------------Shomal Kerman-----------------------------
+
+SELECT
+  Active.LPPostId ActiveId,
+  Active.MPFeederId ActiveMPFeederId,
+  Active.AreaId ActiveAreaId,
+  Active.LPPostName ActiveLPPostName,
+  InActive.LPPostId InActiveId,
+  InActive.MPFeederId InActiveMPFeederId,
+  InActive.AreaId InActiveAreaId,
+  InActive.LPPostName InActiveLPPostName,
+  Active.LPPostCode 
+INTO #tmp
+FROM Tbl_LPPost Active
+INNER JOIN Tbl_LPPost InActive ON Active.LPPostCode = InActive.LPPostCode
+WHERE 
+  Active.IsActive = 1 
+  AND ISNULL(InActive.IsActive, 0) = 0
+  AND ISNULL(Active.LPPostCode , '') <> ''
+
+SELECT 
+  DISTINCT T.ActiveId, 
+  T.LPPostCode, 
+  A.Area,
+  MPF.MPFeederName,
+  MPP.MPPostName
+INTO #tmpAcvtive
+FROM #tmp T
+  INNER JOIN Tbl_MPFeeder MPF ON T.ActiveMPFeederId = MPF.MPFeederId
+  INNER JOIN Tbl_MPPost MPP ON MPF.MPPostId = MPP.MPPostId
+  INNER JOIN Tbl_Area A ON T.ActiveAreaId = A.AreaId
+
+SELECT 
+  DISTINCT T.InActiveId, 
+  T.LPPostCode, 
+  A.Area,
+  MPF.MPFeederName,
+  MPP.MPPostName
+INTO #tmpInAcvtive
+FROM #tmp T
+  INNER JOIN Tbl_MPFeeder MPF ON T.InActiveMPFeederId = MPF.MPFeederId
+  INNER JOIN Tbl_MPPost MPP ON MPF.MPPostId = MPP.MPPostId
+  INNER JOIN Tbl_Area A ON T.InActiveAreaId = A.AreaId
+
+SELECT
+  T.LPPostCode,
+  T.ActiveId,
+  T.InActiveId,
+  T.ActiveLPPostName,
+  TA.Area AS ActiveArea,
+  TA.MPFeederName AS ActiveMPFeederName,
+  TA.MPPostName AS ActiveMPPostName,
+  T.InActiveLPPostName,
+  TI.Area AS InActiveArea,
+  TI.MPFeederName AS InActiveMPFeederName,
+  TI.MPPostName AS InActiveMPPostName
+FROM #tmp T
+  INNER JOIN #tmpAcvtive TA ON T.ActiveId = TA.ActiveId
+  INNER JOIN #tmpInAcvtive TI ON T.InActiveId = TI.InActiveId
+Order By T.InActiveAreaId , T.InActiveMPFeederId
+
+--SELECT * FROM  #tmp 
+DROP TABLE #tmp
+DROP TABLE #tmpAcvtive
+DROP TABLE #tmpInAcvtive
+
+----------------------------------------------
---
+++ Fucking SQLs/شمال کرمان/1401-04-06/STEP 2_ShomalKerman_LPPostLoad.sql
@@ -0,0 +1,32 @@
+﻿
+DECLARE @ActiveId AS INT,
+        @InActiveId AS INT
+DECLARE db_cursor CURSOR FOR 
+SELECT
+  Active.LPPostId ActiveId,
+  InActive.LPPostId InActiveId
+FROM Tbl_LPPost Active
+INNER JOIN Tbl_LPPost InActive ON Active.LPPostCode = InActive.LPPostCode
+WHERE 
+  Active.IsActive = 1 
+  AND ISNULL(InActive.IsActive, 0) = 0
+  AND ISNULL(Active.LPPostCode , '') <> ''
+
+OPEN db_cursor  
+FETCH NEXT FROM db_cursor INTO @ActiveId , @InActiveId  
+
+WHILE @@FETCH_STATUS = 0  
+BEGIN  
+
+    EXEC spChangeLPPost @aFromLPPostId = @InActiveId
+                       ,@aToLPPostId = @ActiveId
+                       ,@IsDelete = 0
+                       ,@IsMergeLPFeeder = 0
+                       ,@IsMergeLPTrans = 0
+                       ,@IsReplicated = 0
+
+      FETCH NEXT FROM db_cursor INTO @ActiveId , @InActiveId  
+END 
+
+CLOSE db_cursor  
+DEALLOCATE db_cursor 
