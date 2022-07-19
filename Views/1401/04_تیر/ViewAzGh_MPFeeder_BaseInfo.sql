2074a3574e9c9560dc0698e4954a4aaa99d3eb70
Omid Dadvar <omiddadvar94@gmail.com>
2022-07-19 09:22:58 +0430

    VIEWs updated :grin:

---
+++ Views/1401/04_تیر/ViewAzGh_MPFeeder_BaseInfo.sql
@@ -0,0 +1,23 @@
+﻿CREATE VIEW ViewAzGh_MPFeeder_BaseInfo
+As
+SELECT A.AreaId 
+      ,A.Area
+      ,MPP.MPPostId 
+      ,MPP.MPPostName 
+      ,MPP.MPPostCode 
+      ,MPP.IsActive AS IsMPPostActive
+      ,MPF.MPFeederId 
+      ,MPF.MPFeederName 
+      ,MPF.MPFeederCode 
+      ,MPF.IsPrivate AS IsMPFeederPrivate
+      ,MPF.IsActive AS IsMPFeederActive
+FROM Tbl_MPFeeder MPF
+  INNER JOIN Tbl_MPPost MPP ON MPF.MPPostId = MPP.MPPostId
+  INNER JOIN Tbl_Area A ON MPF.AreaId = A.AreaId
+ 
+/*
+
+SELECT * FROM ViewAzGh_MPFeeder_BaseInfo
+
+
+*/
