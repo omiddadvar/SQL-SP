48db68d1279d68ad6bec954840bc7b89e9423ea2
Omid Dadvar <omiddadvar94@gmail.com>
2022-09-07 12:17:20 +0430

    spFindCurrentAndFutureOutageByBillingID Debugged ...

--- Stored Procedures/WebServices/1401/05_مرداد/[CREATE] spFindCurrentAndFutureOutageByBillingID.sql
+++ Stored Procedures/WebServices/1401/05_مرداد/[CREATE] spFindCurrentAndFutureOutageByBillingID.sql
@@ -18,7 +18,7 @@ CREATE PROCEDURE spFindCurrentAndFutureOutageByBillingID
       R.DataEntryDTPersian,
       R.Address,
       AU.UserName,
-      CASE WHEN R.IsTamir = 1 THEN ReasonFor.DisconnectLPRequestFor ELSE CR.CallReason END DisconnectGroupSet
+      CR.CallReason AS DisconnectGroupSet
     INTO  #tmpCurrent
     FROM TblRequest R
       INNER JOIN Tbl_AreaUser AU ON R.AreaUserId = AU.AreaUserId
@@ -26,8 +26,7 @@ CREATE PROCEDURE spFindCurrentAndFutureOutageByBillingID
       LEFT JOIN Tbl_DisconnectLPRequestFor ReasonFor ON R.DisconnectRequestForId = ReasonFor.DisconnectLPRequestForId
     WHERE IsTamir = 0 AND 
           DisconnectDatePersian BETWEEN @aFromDate AND @aToDate
-          AND R.EndJobStateId = 5
-          AND (R.LPRequestId IS NOT NULL OR R.MPRequestId IS NOT NULL)
+          AND R.EndJobStateId IN(4,5)
     
     SELECT R.RequestId, 
       R.RequestNumber,
@@ -40,17 +39,21 @@ CREATE PROCEDURE spFindCurrentAndFutureOutageByBillingID
       R.DataEntryDTPersian,
       Address,
       AU.UserName,
-      Sub.TamirRequestSubject AS DisconnectGroupSet
+     CASE WHEN Tr.TamirRequestId IS NULL THEN ReasonFor.DisconnectLPRequestFor ELSE Sub.TamirRequestSubject END DisconnectGroupSet
     INTO  #tmpFuture
     FROM TblRequest R
       INNER JOIN Tbl_AreaUser AU ON R.AreaUserId = AU.AreaUserId
-      INNER JOIN TblTamirRequestConfirm C ON R.RequestId = C.RequestId
-      INNER JOIN TblTamirRequest TR ON C.TamirRequestId = TR.TamirRequestId
+      LEFT JOIN TblTamirRequestConfirm C ON R.RequestId = C.RequestId
+      LEFT JOIN TblTamirRequest TR ON C.TamirRequestId = TR.TamirRequestId
       LEFT JOIN Tbl_TamirRequestSubject Sub ON TR.TamirRequestSubjectId = Sub.TamirRequestSubjectId
+      LEFT JOIN Tbl_DisconnectLPRequestFor ReasonFor ON R.DisconnectRequestForId = ReasonFor.DisconnectLPRequestForId
     WHERE IsTamir = 1 AND 
           R.TamirDisconnectFromDatePersian BETWEEN @aFromDate AND @aToDate
-          AND R.EndJobStateId = 4
-    
+          AND (
+			(R.EndJobStateId = 5 AND (R.LPRequestId IS NOT NULL OR R.MPRequestId IS NOT NULL))
+			OR
+			(R.EndJobStateId = 4) 
+			)
     SELECT TEMP.* INTO #tmpReq FROM (
         SELECT * FROM #tmpCurrent 
           UNION ALL
@@ -68,17 +71,15 @@ CREATE PROCEDURE spFindCurrentAndFutureOutageByBillingID
       AND (LEN(@aBillingId) = 0 OR I.BillingID = @aBillingId)
     ORDER BY T.DisconnectDatePersian DESC , T.DisconnectTime DESC
     
-    
     DROP Table #tmpReq
-
   END
 
 /*
 
 EXEC spFindCurrentAndFutureOutageByBillingID @aFromDate = '1401/01/01',
-                                             @aToDate = '1401/05/30',
+                                             @aToDate = '1401/06/30',
                                              @aAreaId = -1,
-                                             @aBillingId = '1234567890123'
+                                             @aBillingId = '123456789'
 
 */
 
